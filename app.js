da3a82c256a80f103a7fead4870dafaa61af209e
javeedsocial <78414554+javeedsocial@users.noreply.github.com>
2023-11-19 16:33:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
